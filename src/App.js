3157e366f90855209dc236e8f68c788a392314d8
Nidhi <nidhi0613@gmail.com>
2016-03-29 16:42:49 +0530

    test travis

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default class App extends Component {
     return (
       <div className="commentBox">
           <Random inputA={5} inputB={6} />
-          Hello, world! I am a CommentBox.
+          Hello, Nidhi! I am a CommentBox.
       </div>
     );
   }
